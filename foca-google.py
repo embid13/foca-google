f6b25a329e3a92caf6bf7a87be59b3e255bd4d93
Mikel Aristu Kortazar <mikel.aristu@alumni.mondragon.edu>
2024-06-27 11:34:55 +0200

    Upgrade selenium library and code

--- foca-google.py
+++ foca-google.py
@@ -1,113 +1,97 @@
 #!/usr/bin/env python
 
-import time
 import sys
 import os
-import re
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 EXTENSIONS = [
-    'pdf',
-    'doc',
-    'docx',
-    'rtf',
-    'ppt',
-    'pps',
-    'pptx',
-    'ppsx',
-    'xls',
-    'xlsx',
-    'sxw',
-    'sxc',
-    'sxi',
-    'odt',
-    'ods',
-    'odg',
-    'odp',
-    'wpd',
-    'indd',
-    'rdp',
-    'ica',
-    'rar',
-    'txt',
-    'xml',
-    '7z',
-    'zipx'
-];
-
+    'pdf', 'doc', 'docx', 'rtf', 'env', 'bak', 'sql', 'ppt', 'pps', 'pptx', 'ppsx',
+    'xls', 'xlsx', 'sxw', 'sxc', 'sxi', 'odt', 'ods', 'odg', 'odp', 'wpd', 'indd',
+    'rdp', 'ica', 'rar', 'txt', 'xml', '7z', 'zipx'
+]
 
 def make_search_query(domain, extensions):
-    params = ' OR '.join(map(lambda extension : 'filetype:' + extension, extensions))
+    params = ' OR '.join(map(lambda extension: 'filetype:' + extension, extensions))
     if len(extensions) > 1:
         params = '(' + params + ')'
-    return 'site:{0} {1}'.format(domain, params)
-
+    return f'site:{domain} {params}'
 
 def parse_google_serp(query):
-    primary_url = 'https://www.google.com/search?num=100&start=0&hl=em&meta=&q={0}&filter=0'.format(query)
+    primary_url = f'https://www.google.com/search?num=100&start=0&hl=en&meta=&q={query}&filter=0'
 
-    print('\nInitializing Chrome...');
-    print('\nOpening ' + primary_url);
+    print('\nInitializing Chrome...')
+    print(f'\nOpening {primary_url}')
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option('detach', True)
-    # chrome_options.add_argument('user-data-dir={0}/Library/Application Support/Google/Chrome'.format(os.path.expanduser('~')))
+    chrome_options.add_argument('user-data-dir=/home/mikel/.config/BraveSoftware/Brave-Browser/Library/Application Support/Google/Chrome')
     browser = webdriver.Chrome(options=chrome_options)
 
     browser.get(primary_url)
-    time.sleep(3)
 
-    is_captcha = len(browser.find_elements_by_css_selector('#captcha-form'))
-    if is_captcha:
+    # Use explicit wait to check for the CAPTCHA form
+    try:
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#captcha-form')))
         sys.exit('\nError: captcha detected. Solve captcha and run script again.')
+    except TimeoutException:
+        pass
 
-    results = parse_google_serp_single_page(browser);
-    print('\nFound {0} files in Google. Waiting...'.format(len(results)))
+    results = parse_google_serp_single_page(browser)
+    print(f'\nFound {len(results)} files in Google. Waiting...')
 
-    pages = list(map(lambda page : page.get_attribute('href'), browser.find_elements_by_css_selector('#nav td > a.fl')))
-
-    if len(pages):
-        print('Detected {0} additional search results pages'.format(len(pages)))
+    # Find pagination links
+    pages = [page.get_attribute('href') for page in browser.find_elements(By.CSS_SELECTOR, '#nav td a.fl')]
+    if pages:
+        print(f'Detected {len(pages)} additional search results pages')
         for page in pages:
             browser.get(page)
-            time.sleep(3)
+            WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.r > a')))
             page_results = parse_google_serp_single_page(browser)
-            print('Found {0} more files in Google. Waiting...'.format(len(page_results)))
+            print(f'Found {len(page_results)} more files in Google. Waiting...')
             results.extend(page_results)
 
     browser.quit()
-    return results;
-
+    return results
 
 def parse_google_serp_single_page(browser):
-    is_captcha = len(browser.find_elements_by_css_selector('#captcha-form'))
-    if is_captcha:
-        return [];
-    return list(map(lambda res : res.get_attribute('href'), browser.find_elements_by_css_selector('.r > a:not(.fl)')))
-
+    anchor_elements = browser.find_elements(By.TAG_NAME,'a')
+    links = [anchor.get_attribute('href') for anchor in anchor_elements if anchor.get_attribute('href') is not None]
+    return links
 
 def save_log_file(results, domain, filename):
-    filepath = '{0}/{1}'.format(domain, filename)
-    os.system('touch ./' + filepath)
-    file = open(filepath, 'w+')
-    file.write('\n'.join(results))
-    file.close()
-    print('Saved results to {0} ({1} files total)'.format(filepath, len(results)))
+    filepath = f'{domain}/{filename}'
+    os.makedirs(domain, exist_ok=True)
+    with open(filepath, 'w') as file:
+        file.write('\n'.join(results))
+    print(f'Saved results to {filepath} ({len(results)} files total)')
 
 
 def download_files(results, domain, filename):
-    print('\n\n\nDownloading {0} files from {1}/{2} into {1} folder...\n\n\n'.format(len(results), domain, filename))
-    os.system('wget -i ./{0}/{1} -P ./{0}'.format(domain, filename))
-    print('\n\n\nSuccessfully downloaded {0} files into {1} folder\n\n\n'.format(len(results), domain))
+    print(f'\n\n\nDownloading {len(results)} files from {domain}/{filename} into {domain} folder...\n\n\n')
+    with open(f'./{domain}/{filename}', 'r') as file:
+        links = file.readlines()
+    
+    filtered_links = [link.strip() for link in links if domain in link]
 
+    temp_filename = f'./{domain}/temp_log.txt'
+    with open(temp_filename, 'w') as temp_file:
+        temp_file.write('\n'.join(filtered_links))
+    
+    os.system(f'wget -i {temp_filename} -P ./{domain}')
+    
+    os.remove(temp_filename)
+    
+    print(f'\n\n\nSuccessfully downloaded {len(filtered_links)} files into {domain} folder\n\n\n')
 
 def init_csv_search():
-    print ('\nSearching public repositories on {0}...'.format(domain))
+    print(f'\nSearching public repositories on {domain}...')
     query = make_search_query(domain, ['git', 'svn'])
     repositories = parse_google_serp(query)
     save_log_file(repositories, domain, '0_repositories.txt')
-
 
 if len(sys.argv) == 1:
     print("""
@@ -133,9 +117,9 @@ else:
     else:
         query = make_search_query(domain, EXTENSIONS)
 
-    os.system('mkdir ./{0}'.format(domain))
+    os.makedirs(domain, exist_ok=True)
 
-    print('\nSearching {0} files on {1}'.format('all static' if not extension else extension.upper(), domain))
+    print(f'\nSearching {"all static" if not extension else extension.upper()} files on {domain}')
     results = parse_google_serp(query)
     save_log_file(results, domain, '0_log.txt')
     download_files(results, domain, '0_log.txt')
